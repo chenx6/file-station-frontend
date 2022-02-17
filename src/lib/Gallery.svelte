3991fa6aed92278fc260553f58029510ae7cad02
chen_null <abc82766@gmail.com>
2022-02-17 13:49:01 +0800

    Removing gallery cycling

--- src/lib/Gallery.svelte
+++ src/lib/Gallery.svelte
@@ -7,7 +7,7 @@
   let activeIndex = files.findIndex((v) => v.name === selected.name);
 </script>
 
-<Carousel {items} bind:activeIndex>
+<Carousel {items} bind:activeIndex interval={false}>
   <div class="carousel-inner">
     {#each items as item, index}
       <CarouselItem bind:activeIndex itemIndex={index}>
